3977649247baa526e37e3a063791d105fb82d8f3
Ashish Baraiya <ashishba@cybage.com>
2015-04-16 14:57:19 +0530

    MObile automation using selendroid

---
+++ src/com/mobileauto/test/SelendroidCapabilitiesTest.java
@@ -0,0 +1,47 @@
+package com.mobileauto.test;
+
+import io.selendroid.client.SelendroidDriver;
+import io.selendroid.common.SelendroidCapabilities;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SelendroidCapabilitiesTest {
+	 
+      @Test()
+      public void APKTest() throws Exception {
+    	  SelendroidCapabilities capa = new SelendroidCapabilities("io.selendroid.testapp:0.15.0");
+
+    	  WebDriver driver = new SelendroidDriver(capa);
+    	  WebElement inputField = driver.findElement(By.id("my_text_field"));
+    	  Assert.assertEquals("true", inputField.getAttribute("enabled"));
+    	  inputField.sendKeys("Selendroid");
+    	  Assert.assertEquals("Selendroid", inputField.getText());
+    	  driver.quit();
+      }
+      
+      @Test()
+      public void MobileWeb() throws Exception {
+    	  SelendroidCapabilities capa = new SelendroidCapabilities("android");
+    	  WebDriver driver = new SelendroidDriver(capa);
+    	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);    	  
+    	  
+//    	  WebDriver driver = new FirefoxDriver();
+    	  
+    	  driver.get("https://www.google.com");
+    	  WebElement element1 = driver.findElement(By.name("q"));
+    	  element1.click();
+    	  element1.sendKeys("This is my MobileWeb Automation.");
+    	  driver.findElement(By.name("btnG")).click();
+    	  Thread.sleep(10000);
+    	  driver.findElement(By.xpath(".//*[@id='rso']/descendant::a[1]")).click();
+    	  Thread.sleep(10000);
+    	  Assert.assertEquals(driver.getTitle(), "The Basics Of Test Automation For Apps, Games, Mobile Web");
+    	  driver.quit();
+      }
+      
+      
+}
